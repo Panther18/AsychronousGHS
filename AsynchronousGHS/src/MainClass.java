16244b2e9087f47c79597d52b19ed6f51e2b46ab
Pavan Yadiki <mail2pavanyr@gmail.com>
2015-03-01 00:44:34 -0600

    Initial Commit.

---
+++ AsynchronousGHS/src/MainClass.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Uploading to git...");
+		Master objMaster=new Master();
+		objMaster.setMasterObject(objMaster);
+		try {
+			objMaster.getInput();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("***ERROR: while reading input from file");
+			e.printStackTrace();
+		}
+		objMaster.masterThread("Master");
+	}
+}
